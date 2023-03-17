f04c917c52efc2a672b9004657d6310bc6b9b032
Brian Shaw <1BrianShaw@gmail.com>
2023-03-17 12:06:26 -0400

    Update Product

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Testing.Models;
 
 namespace Testing
@@ -10,5 +7,6 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
     }
 }
